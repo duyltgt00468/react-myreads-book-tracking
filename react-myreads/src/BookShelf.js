bb17d165bcfd0e01ac82b8164c6c96dca5ffb494
DuyLT <luutheduy2@gmail.com>
2023-06-17 11:37:34 +0700

    dev | Add book types

---
+++ react-myreads/src/BookShelf.js
@@ -0,0 +1,7 @@
+const bookShelf = [
+    { id: 'currentlyReading', title: 'Currently reading' },
+    { id: 'wantToRead', title: 'Want to read' },
+    { id: 'read', title: 'Read' }
+];
+
+export default bookShelf;
